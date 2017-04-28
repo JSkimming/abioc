8c7f60cf8972a439281cd102d38de146c3ec9385
James Skimming <JSkimming@users.noreply.github.com>
2017-04-28 10:23:56 +0100

    Removed redundant code

--- src/Abioc/Composition/VisitorFactory.cs
+++ src/Abioc/Composition/VisitorFactory.cs
@@ -88,6 +88,9 @@ namespace Abioc.Composition
                 .ToArray();
         }
 
+        //// The following is redundant code, though I suspect some of it may make a return, so I'm keeping it here for
+        //// reference. I plan to clean this up at some point.
+
         ////private static Type[] GetTypedVisitorTypes()
         ////{
         ////    IEnumerable<Type> types =
@@ -129,6 +132,103 @@ namespace Abioc.Composition
         ////        select t;
 
         ////    return types.ToArray();
+        ////}
+
+        ////private static Dictionary<Type, List<IRegistrationVisitor>> GetVisitors(
+        ////    CompositionContext context)
+        ////{
+        ////    Assembly assembly = typeof(IRegistrationVisitor).GetTypeInfo().Assembly;
+        ////    IEnumerable<Type> visitorTypes =
+        ////        from t in assembly.GetTypes().Where(typeof(IRegistrationVisitor).GetTypeInfo().IsAssignableFrom)
+        ////        let ti = t.GetTypeInfo()
+        ////        where ti.IsClass && !ti.IsAbstract
+        ////        select t;
+
+        ////    var visitors = new Dictionary<Type, List<IRegistrationVisitor>>();
+
+        ////    foreach (var visitorType in visitorTypes)
+        ////    {
+        ////        TypeInfo typeInfo = visitorType.GetTypeInfo();
+
+        ////        IEnumerable<Type> interfaceTypes =
+        ////            typeInfo
+        ////                .GetInterfaces()
+        ////                .Where(i => i.GetTypeInfo().IsGenericType
+        ////                            && typeof(IRegistrationVisitor<>) == i.GetGenericTypeDefinition());
+
+        ////        if (typeInfo.ContainsGenericParameters)
+        ////        {
+        ////            Type[] parameters = typeInfo.GenericTypeParameters;
+        ////            Type[] arguments = typeInfo.GenericTypeArguments;
+        ////        }
+
+        ////        var visitor = (IRegistrationVisitor)Activator.CreateInstance(visitorType);
+        ////        visitor.Initialize(context);
+
+        ////        foreach (Type interfaceType in interfaceTypes)
+        ////        {
+        ////            List<IRegistrationVisitor> list;
+        ////            if (!visitors.TryGetValue(interfaceType, out list))
+        ////            {
+        ////                list = new List<IRegistrationVisitor>(1);
+        ////                visitors[interfaceType] = list;
+        ////            }
+
+        ////            list.Add(visitor);
+        ////        }
+        ////    }
+
+        ////    return visitors;
+        ////}
+
+        ////private static IRegistrationVisitor CreateVisitor(Type visitorType)
+        ////{
+        ////    if (visitorType == null)
+        ////        throw new ArgumentNullException(nameof(visitorType));
+
+        ////    var visitor = (IRegistrationVisitor)Activator.CreateInstance(visitorType);
+        ////    return visitor;
+        ////}
+
+        ////private static IRegistrationVisitor CreateVisitorWithContext(Type visitorType, Type extraType)
+        ////{
+        ////    if (visitorType == null)
+        ////        throw new ArgumentNullException(nameof(visitorType));
+        ////    if (extraType == null)
+        ////        throw new ArgumentNullException(nameof(extraType));
+
+        ////    TypeInfo typeInfo = visitorType.GetTypeInfo();
+        ////    Type genericType = typeInfo.MakeGenericType(extraType);
+        ////    return CreateVisitor(genericType);
+        ////}
+
+        ////private static IRegistrationVisitor CreateTypedVisitor(Type visitorType, Type implementationType)
+        ////{
+        ////    if (visitorType == null)
+        ////        throw new ArgumentNullException(nameof(visitorType));
+        ////    if (implementationType == null)
+        ////        throw new ArgumentNullException(nameof(implementationType));
+
+        ////    TypeInfo typeInfo = visitorType.GetTypeInfo();
+        ////    Type genericType = typeInfo.MakeGenericType(implementationType);
+        ////    return CreateVisitor(genericType);
+        ////}
+
+        ////private static IRegistrationVisitor CreateTypedVisitorWithContext(
+        ////    Type visitorType,
+        ////    Type extraType,
+        ////    Type implementationType)
+        ////{
+        ////    if (visitorType == null)
+        ////        throw new ArgumentNullException(nameof(visitorType));
+        ////    if (extraType == null)
+        ////        throw new ArgumentNullException(nameof(extraType));
+        ////    if (implementationType == null)
+        ////        throw new ArgumentNullException(nameof(implementationType));
+
+        ////    TypeInfo typeInfo = visitorType.GetTypeInfo();
+        ////    Type genericType = typeInfo.MakeGenericType(extraType, implementationType);
+        ////    return CreateVisitor(genericType);
         ////}
     }
 }
