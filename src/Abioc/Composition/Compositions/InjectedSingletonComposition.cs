8e58a8619435fa5457c59a4d04abc45ed1bc33fe
James Skimming <JSkimming@users.noreply.github.com>
2017-04-30 15:00:15 +0100

    Added the injected singleton visitor and composition

---
+++ src/Abioc/Composition/Compositions/InjectedSingletonComposition.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) 2017 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Abioc.Composition.Compositions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    /// <summary>
+    /// A composition to produce code for a injected value.
+    /// </summary>
+    /// <typeparam name="TImplementation">The type of the injected <see cref="Value"/>.</typeparam>
+    public class InjectedSingletonComposition<TImplementation> : CompositionBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InjectedSingletonComposition{TImplementation}"/> class.
+        /// </summary>
+        /// <param name="value">
+        /// The <see cref="Value"/> of type <typeparamref name="TImplementation"/>.
+        /// </param>
+        public InjectedSingletonComposition(TImplementation value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Gets the type of the <see cref="Value"/>.
+        /// </summary>
+        public override Type Type => typeof(TImplementation);
+
+        /// <summary>
+        /// Gets the injected value.
+        /// </summary>
+        public TImplementation Value { get; }
+
+        /// <inheritdoc/>
+        public override string GetComposeMethodName(CompositionContext context, bool simpleName)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            string methodName = "Accessor" + Type.ToCompileMethodName(simpleName);
+            return methodName;
+        }
+
+        /// <inheritdoc/>
+        public override IEnumerable<string> GetMethods(CompositionContext context, bool simpleName)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            string methodName = GetComposeMethodName(context, simpleName);
+            string instanceExpression = GetInstanceExpression(context, simpleName);
+
+            string method =
+                Type.GetTypeInfo().IsValueType
+                    ? string.Format(
+                        "private static object {0}(){1}{{{1}    return (object){2};{1}}}",
+                        methodName,
+                        Environment.NewLine,
+                        instanceExpression)
+                    : string.Format(
+                        "private static {0} {1}(){2}{{{2}    return {3};{2}}}",
+                        Type.ToCompileName(),
+                        methodName,
+                        Environment.NewLine,
+                        instanceExpression);
+
+            return new[] { method };
+        }
+
+        /// <inheritdoc />
+        public override string GetInstanceExpression(CompositionContext context, bool simpleName)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            string injectedFieldName = GetInjectedFieldName();
+            return injectedFieldName;
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<(string snippet, object value)> GetFieldInitializations(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            string fieldName = GetInjectedFieldName();
+            string fieldType = Type.ToCompileName();
+
+            string snippet = $"{fieldName} = ({fieldType})";
+            return new[] { (snippet, (object)Value) };
+        }
+
+        /// <inheritdoc />
+        public override IEnumerable<string> GetFields(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            string fieldName = GetInjectedFieldName();
+            string fieldType = Type.ToCompileName();
+
+            string field = $"private static {fieldType} {fieldName};";
+            return new[] { field };
+        }
+
+        /// <inheritdoc/>
+        public override bool RequiresConstructionContext(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            return RequiresConstructionContext();
+        }
+
+        private bool RequiresConstructionContext() => false;
+
+        private string GetInjectedFieldName() => "Injected_" + Type.ToCompileMethodName(simpleName: false);
+    }
+}
---
+++ src/Abioc/Composition/Visitors/InjectedSingletonRegistrationVisitor.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) 2017 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Abioc.Composition.Visitors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Abioc.Composition.Compositions;
+    using Abioc.Registration;
+
+    /// <summary>
+    /// A composition registration visitor for a <see cref="InjectedSingletonRegistration{TImplementation}"/>.
+    /// </summary>
+    /// <typeparam name="TImplementation">The <see cref="IRegistration.ImplementationType"/>.</typeparam>
+    public class InjectedSingletonRegistrationVisitor<TImplementation>
+        : IRegistrationVisitor<InjectedSingletonRegistration<TImplementation>>
+    {
+        private CompositionContext _context;
+
+        /// <summary>
+        /// Initializes the <see cref="IRegistrationVisitor"/>.
+        /// </summary>
+        /// <param name="context">The composition context.</param>
+        public void Initialize(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            _context = context;
+        }
+
+        /// <summary>
+        /// Accepts the <see cref="InjectedSingletonRegistration{TImplementation}"/> to visit.
+        /// </summary>
+        /// <param name="registration">
+        /// The <see cref="InjectedSingletonRegistration{TImplementation}"/> to visit.
+        /// </param>
+        public void Accept(InjectedSingletonRegistration<TImplementation> registration)
+        {
+            if (registration == null)
+                throw new ArgumentNullException(nameof(registration));
+
+            IComposition composition = new InjectedSingletonComposition<TImplementation>(registration.Value);
+            _context.Compositions[composition.Type] = composition;
+        }
+    }
+}
