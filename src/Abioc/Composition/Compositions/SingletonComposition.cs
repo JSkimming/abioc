3e68d3912d22ad3e05d0ca0b98e1974c27f8aaf7
James Skimming <JSkimming@users.noreply.github.com>
2017-05-04 22:01:53 +0100

    Added singleton visitor and composition

---
+++ src/Abioc/Composition/Compositions/SingletonComposition.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) 2017 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Abioc.Composition.Compositions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// A composition to produce code for a singleton value.
+    /// </summary>
+    public class SingletonComposition : IComposition
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SingletonComposition"/> class.
+        /// </summary>
+        /// <param name="inner">The <see cref="Inner"/> <see cref="IComposition"/>.</param>
+        public SingletonComposition(IComposition inner)
+        {
+            if (inner == null)
+                throw new ArgumentNullException(nameof(inner));
+
+            Inner = inner;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Inner"/> <see cref="IComposition"/>.
+        /// </summary>
+        public IComposition Inner { get; }
+
+        /// <summary>
+        /// Gets the type provided by the <see cref="Inner"/> <see cref="IComposition"/>.
+        /// </summary>
+        public Type Type => Inner.Type;
+
+        /// <inheritdoc />
+        public string GetInstanceExpression(CompositionContext context, bool simpleName)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            return Inner.GetInstanceExpression(context, simpleName);
+        }
+
+        /// <inheritdoc />
+        public string GetComposeMethodName(CompositionContext context, bool simpleName)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            return Inner.GetComposeMethodName(context, simpleName);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<string> GetMethods(CompositionContext context, bool simpleName)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            return Inner.GetMethods(context, simpleName);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<string> GetFields(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            return Inner.GetFields(context);
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<(string snippet, object value)> GetFieldInitializations(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            return Inner.GetFieldInitializations(context);
+        }
+
+        /// <inheritdoc />
+        public bool RequiresConstructionContext(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            return Inner.RequiresConstructionContext(context);
+        }
+    }
+}
---
+++ src/Abioc/Composition/Visitors/SingletonRegistrationVisitor.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) 2017 James Skimming. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace Abioc.Composition.Visitors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Abioc.Composition.Compositions;
+    using Abioc.Registration;
+
+    /// <summary>
+    /// A composition registration visitor for a <see cref="SingletonRegistration"/>.
+    /// </summary>
+    public class SingletonRegistrationVisitor : IRegistrationVisitor<SingletonRegistration>, IRegistrationVisitorEx
+    {
+        private CompositionContext _context;
+
+        private VisitorManager _manager;
+
+        /// <summary>
+        /// Initializes the <see cref="IRegistrationVisitor"/>.
+        /// </summary>
+        /// <param name="context">The composition context.</param>
+        public void Initialize(CompositionContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            _context = context;
+        }
+
+        /// <summary>
+        /// Accepts the <see cref="SingletonRegistration"/> to visit.
+        /// </summary>
+        /// <param name="registration">The <see cref="SingletonRegistration"/> to visit.</param>
+        public void Accept(SingletonRegistration registration)
+        {
+            if (registration == null)
+                throw new ArgumentNullException(nameof(registration));
+
+            _manager.Visit(registration.Inner);
+
+            // Get the original composition.
+            IComposition inner = _context.Compositions[registration.ImplementationType];
+
+            // Replace the inner composition.
+            IComposition composition = new SingletonComposition(inner);
+            _context.Compositions[composition.Type] = composition;
+        }
+
+        /// <inheritdoc />
+        void IRegistrationVisitorEx.InitializeEx(VisitorManager manager)
+        {
+            if (manager == null)
+                throw new ArgumentNullException(nameof(manager));
+
+            _manager = manager;
+        }
+    }
+}
