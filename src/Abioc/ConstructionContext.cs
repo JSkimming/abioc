7e775d0b498e1c2895bcb8f13832a37dc3d3ad68
James Skimming <JSkimming@users.noreply.github.com>
2017-05-22 19:09:09 +0100

    Use readonly fields on ConstructionContext

--- src/Abioc/ConstructionContext.cs
+++ src/Abioc/ConstructionContext.cs
@@ -20,6 +20,34 @@ namespace Abioc
         public static readonly ConstructionContext<TExtra> Default = default(ConstructionContext<TExtra>);
 
         /// <summary>
+        /// Gets the <see cref="Type"/> of the service to be provided that satisfies the
+        /// <see cref="ServiceType"/>.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="ImplementationType"/> should be assignable to the <see cref="ServiceType"/>
+        /// </remarks>
+        public readonly Type ImplementationType;
+
+        /// <summary>
+        /// Gets the <see cref="Type"/> of the requested service.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="ServiceType"/> should be satisfied by being
+        /// <see cref="TypeInfo.IsAssignableFrom(TypeInfo)"/> the <see cref="ImplementationType"/>.
+        /// </remarks>
+        public readonly Type ServiceType;
+
+        /// <summary>
+        /// Gets the type of the component into which the service <see cref="ServiceType"/> is injected.
+        /// </summary>
+        public readonly Type RecipientType;
+
+        /// <summary>
+        /// Gets the <see cref="Extra"/> construction context information to be propitiated through all construction.
+        /// </summary>
+        public readonly TExtra Extra;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="ConstructionContext{TExtra}"/> struct.
         /// </summary>
         /// <param name="implementationType">The <see cref="ImplementationType"/>.</param>
@@ -37,33 +65,5 @@ namespace Abioc
             RecipientType = recipientType;
             Extra = extra;
         }
-
-        /// <summary>
-        /// Gets the <see cref="Type"/> of the service to be provided that satisfies the
-        /// <see cref="ServiceType"/>.
-        /// </summary>
-        /// <remarks>
-        /// The <see cref="ImplementationType"/> should be assignable to the <see cref="ServiceType"/>
-        /// </remarks>
-        public Type ImplementationType { get; }
-
-        /// <summary>
-        /// Gets the <see cref="Type"/> of the requested service.
-        /// </summary>
-        /// <remarks>
-        /// The <see cref="ServiceType"/> should be satisfied by being
-        /// <see cref="TypeInfo.IsAssignableFrom(TypeInfo)"/> the <see cref="ImplementationType"/>.
-        /// </remarks>
-        public Type ServiceType { get; }
-
-        /// <summary>
-        /// Gets the type of the component into which the service <see cref="ServiceType"/> is injected.
-        /// </summary>
-        public Type RecipientType { get; }
-
-        /// <summary>
-        /// Gets the <see cref="Extra"/> construction context information to be propitiated through all construction.
-        /// </summary>
-        public TExtra Extra { get; }
     }
 }
