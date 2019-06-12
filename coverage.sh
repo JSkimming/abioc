f54cac1402e290f0f5b8e9626335fdb10aaa89a2
James Skimming <JSkimming@users.noreply.github.com>
2019-06-12 12:08:27 +0100

    :chart_with_upwards_trend: Capture all test output

--- coverage.sh
+++ coverage.sh
@@ -7,13 +7,16 @@ exe() { echo; echo "\$ $*" ; "$@" ; }
 framework="${1-netcoreapp2.1}"
 config="${2-Debug}"
 
-testResults="test/TestResults"
 include="[abioc]*"
 exclude="\"[*.Tests]*,[Abioc.Tests.Internal]*\""
 
 # Cannot use a bash solution in alpine builds https://stackoverflow.com/a/246128
 #rootDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rootDir=$(pwd)
+
+testResults="test/TestResults"
+output="$rootDir/$testResults/output"
+tools="$rootDir/$testResults/tools"
 
 testProj1="$rootDir/test/Abioc.Tests.Internal/Abioc.Tests.Internal.csproj"
 testProj2="$rootDir/test/Abioc.Tests/Abioc.Tests.csproj"
@@ -28,42 +31,42 @@ exe dotnet build --no-restore -f "$framework" -c "$config" "$testProj2"
 # Execute the tests
 exe dotnet test --no-restore --no-build -f "$framework" -c "$config" \
 "$testProj1" \
---results-directory "$rootDir/$testResults/output/" \
+--results-directory "$output/" \
 --logger "\"trx;LogFileName=$(basename "$testProj1" .csproj).trx\"" \
 /p:CollectCoverage=true \
 /p:Include="$include" \
 /p:Exclude="$exclude" \
-/p:CoverletOutput="$rootDir/$testResults/internal.coverage.json"
+/p:CoverletOutput="$output/internal.coverage.json"
 
 exe dotnet test --no-restore --no-build -f "$framework" -c "$config" \
 "$testProj2" \
---results-directory "$rootDir/$testResults/output/" \
+--results-directory "$output/" \
 --logger "\"trx;LogFileName=$(basename "$testProj2" .csproj).trx\"" \
 /p:CollectCoverage=true \
 /p:Include="$include" \
 /p:Exclude="$exclude" \
-/p:MergeWith="$rootDir/$testResults/internal.coverage.json" \
-/p:CoverletOutput="$rootDir/$testResults/" \
-/p:CoverletOutputFormat="\"json,opencover\""
+/p:MergeWith="$output/internal.coverage.json" \
+/p:CoverletOutput="$output/" \
+/p:CoverletOutputFormat="\"json,opencover,cobertura\""
 
 # Install trx2junit if not already installed
-if [ ! -f "$rootDir/$testResults/tools/trx2junit" ]
+if [ ! -f "$tools/trx2junit" ]
 then
-   exe dotnet tool install trx2junit --tool-path "$rootDir/$testResults/tools"
+   exe dotnet tool install trx2junit --tool-path "$tools"
 fi
 
 # Install ReportGenerator if not already installed
-if [ ! -f "$rootDir/$testResults/tools/reportgenerator" ]
+if [ ! -f "$tools/reportgenerator" ]
 then
-   exe dotnet tool install dotnet-reportgenerator-globaltool --tool-path "$rootDir/$testResults/tools"
+   exe dotnet tool install dotnet-reportgenerator-globaltool --tool-path "$tools"
 fi
 
 # Convert the MSTest trx files to junit xml
-exe "$rootDir/$testResults/tools/trx2junit" "$rootDir/$testResults/output"/*.trx
+exe "$tools/trx2junit" "$output"/*.trx
 
 # Generate the reports
-exe "$rootDir/$testResults/tools/reportgenerator" \
+exe "$tools/reportgenerator" \
 "-verbosity:Info" \
-"-reports:$rootDir/$testResults/coverage.opencover.xml" \
-"-targetdir:$rootDir/$testResults/Report" \
+"-reports:$output/coverage.opencover.xml" \
+"-targetdir:$output/Report" \
 "-reporttypes:Html"
